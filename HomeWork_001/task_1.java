eb7998549128b5a17d8c142c43af6effee050077
Sergey <sergey_ageev13@mail.ru>
2023-02-14 19:13:49 +0300

    added method view

--- HomeWork_001/task_1.java
+++ HomeWork_001/task_1.java
@@ -1,5 +1,24 @@
 package HomeWork_001;
 
+import java.util.Scanner;
+import java.util.Random;
+
+
 public class task_1 {
+
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
     
+    static String view (String value) {
+        System.out.println("выбор ввода порядка триугольного число:\n" +
+        "для ввода случайного числа введите 'r'" +
+        "для ручного ввода нажмите введите число натурально число");
+        value = input.nextLine();
+        return value;
+    }
 }
