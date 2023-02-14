b0d6b0eb2eeea0c4ceb99bdff44e1a47d4594afb
Sergey <sergey_ageev13@mail.ru>
2023-02-15 02:02:56 +0300

    added POW

--- HomeWork_002/task_1.java
+++ HomeWork_002/task_1.java
@@ -1,6 +1,12 @@
 package HomeWork_002;
 
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
 public class task_1 {
 
@@ -14,7 +20,7 @@ public class task_1 {
         a = view();
         System.out.println("введите число b:");
         b = view();
-        otvet = math(a, b);
+        otvet = mathAll(a, b);
         System.out.printf(String.format("a = %d\nb = %d\notvet = %f", a, b, otvet));
     }
 
@@ -29,35 +35,48 @@ public class task_1 {
             System.out.println("не корректные данные!\n повторите ввод:");
             return intValue = view();
         }
-
     }
 
-    static double math(int a, int b) {
+    static double mathPOV(int a, int b) {
         int check = 1;
         double res = 0;
+        if (b < 0)
+            b *= (-1);
         while (b > 1) {
-            if (b>2) {
+            if (b > 2) {
                 if (b > 2 & b % 2 == 0) {
                     a = a * a;
                     b /= 2;
-                    System.out.print("%==0 " + "ch = " + check + " a = " + a + " b = " + b + "\n");
                 }
                 if (b > 2 & b % 2 == 1) {
                     check *= a;
                     b -= 1;
-                    System.out.print("%==1 " + "ch = " + check + " a = " + a + " b = " + b + "\n");
-                    // System.out.println(" ");
                 }
             } else {
-                System.out.print("не вошли в иф " + "ch = " + check + " a = " + a + " b = " + b + "\n");
                 res = a * a * check;
                 b = b - 1;
-                System.out.println(" res= " + res + "\n");
             }
-
         }
         return res;
     }
 
-    // static int
+    static double mathAll(int a, int b) {
+        double result = 1;
+        if (b > 1) {
+            result = mathPOV(a, b);
+            return result;
+        }
+        if (b == 1) {
+            return a;
+        }
+        if (b == -1) {
+            result = result / a;
+            return result;
+        }
+        if (b < -1) {
+            result = 1 / mathPOV(a, b);
+            return result;
+        } else
+            return 1;
+    }
 }
